f6dad734246ea31a66dea622511272e31aadd266
Yann Carlen <yann-carlen@hotmail.fr>
2017-11-04 18:01:35 +0100

    add icon generation

--- discovery/src/app.js
+++ discovery/src/app.js
@@ -17,14 +17,14 @@ const getTemperatures = (...town) => Promise.all(town.map(fetchInfos))
 
 const getTownsTemperatures = (...town) => new Promise((s, f) => getTemperatures(...town)
     .then(e => s(e.reduce((acc, cur) => {
-        acc.push({ [cur['city_info'].name]: cur['current_condition'].tmp })
+        acc.push({ [cur['city_info'].name]: { tmp: cur['current_condition'].tmp, img: cur['current_condition'].icon_big } })
         return acc
     }, []))).catch(f))
 
 http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.writeHead(200)
-    getTownsTemperatures('strasbourg', 'paris').then(e => res.end(`<html>
+    getTownsTemperatures('strasbourg', 'paris', 'lyon', 'lille', 'nancy').then(e => res.end(`<html>
         <head>
             <title>Weather | Node.js</title>
             <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
@@ -32,7 +32,12 @@ http.createServer((req, res) => {
         <body>
             <h1>Weather</h1>
             <ul>    
-                ${e.map(t => `<li>${Object.keys(t).pop()} | <strong>${t[Object.keys(t).pop()]}</strong></li>`).join('')} 
+                ${e.map(t => `
+                <li>
+                    ${Object.keys(t).pop()} | <strong>${t[Object.keys(t).pop()].tmp}</strong>
+                    <br/>
+                    <img src="${t[Object.keys(t).pop()].img}" alt=${Object.keys(t).pop()}/>
+                </li>`).join('')} 
             </ul>
         </body>
     </html>`))
